6089c6c479f4d6546029a02acd8043e414855088
Ryan F Venter <msg@rfv.io>
2017-03-23 02:07:02 +0800

    Contract skeleton

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,7 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+var ExampleToken = artifacts.require("./ExampleToken.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  deployer.deploy(ExampleToken);
+  // deployer.link(ConvertLib, MetaCoin);
+  // deployer.deploy(MetaCoin);
 };
